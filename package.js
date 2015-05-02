bc1e28c840c33c4e9a3b25be65e544109f25d6cd
mojun <immojun@gmail.com>
2015-05-02 18:56:03 +0800

    modi version
    modi version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'immojun:rest-accounts-password',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Get a login token to use with simple:rest',
   // URL to the Git repository containing the source code for this package.
